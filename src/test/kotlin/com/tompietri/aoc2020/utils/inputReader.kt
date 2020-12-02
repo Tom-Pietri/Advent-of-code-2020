6bbb46c65e2b71c35a199d94ec1fd6a19539141e
Tom Pietri <tom.pietri@4sh.fr>
2020-12-02 09:05:29 +0100

    Fix wrong folder targeted for reading input

--- src/test/kotlin/com/tompietri/aoc2020/utils/inputReader.kt
+++ src/test/kotlin/com/tompietri/aoc2020/utils/inputReader.kt
@@ -3,7 +3,7 @@ package com.tompietri.aoc2020.utils
 import java.io.File
 
 fun getInputFileForDay(dayNumber: Int) : File {
-    return File("./src/main/kotlin/com/tompietri/aoc2020/day1/day$dayNumber.input")
+    return File("./src/main/kotlin/com/tompietri/aoc2020/day$dayNumber/day$dayNumber.input")
 }
 
 fun readInputWithSingleLine(dayNumber: Int): String {
